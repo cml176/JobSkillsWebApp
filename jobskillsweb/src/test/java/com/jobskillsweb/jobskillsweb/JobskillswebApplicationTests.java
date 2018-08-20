a08c2d179dc4adf360032f283f19d2f7e2facee4
cml176 <cml176@txstate.edu>
2018-08-20 03:13:06 -0500

    test push to development using git from cmd

---
+++ jobskillsweb/src/test/java/com/jobskillsweb/jobskillsweb/JobskillswebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jobskillsweb.jobskillsweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JobskillswebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
