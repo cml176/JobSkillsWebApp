f42b3c4d655f5cf280a7992d0f9293f5fad983ef
cml176 <cml176@txstate.edu>
2018-08-20 03:04:28 -0500

    first commit

---
+++ jobskillsweb/src/main/java/com/jobskillsweb/main/JobskillswebApplication.java
@@ -0,0 +1,12 @@
+package com.jobskillsweb.jobskillsweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobskillswebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobskillswebApplication.class, args);
+	}
+}
